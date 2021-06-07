258823688136a569aaad6d2a3d5666a6e7d541e0
Akseli Palen <akseli.palen@gmail.com>
2021-06-07 22:56:20 +0300

    Test ROT45

--- test/transform/getRotation.js
+++ test/transform/getRotation.js
@@ -2,12 +2,14 @@ const nudged = require('../../index')
 const getRotation = nudged.transform.getRotation
 
 const multiply = nudged.transform.multiply
+const ROT45 = nudged.transform.ROT45
 const ROT90 = nudged.transform.ROT90
 const ROT180 = nudged.transform.ROT180
 
 module.exports = (ts) => {
   ts.test('case: basic rotations', (t) => {
     t.almostEqual(getRotation(ROT90), Math.PI / 2)
+    t.almostEqual(getRotation(ROT45), Math.PI / 4)
 
     const double90 = multiply(ROT90, ROT90)
     t.almostEqual(
