beef0681b6c838540747906ca87b8e7a40ef1f90
Akseli Palén <akseli.palen@gmail.com>
2021-06-07 14:58:22 +0300

    Custom assertion method: transformEqual

--- test/index.js
+++ test/index.js
@@ -1,9 +1,20 @@
 const test = require('tape')
+const almostEqual = require('../lib/transform').almostEqual
 
 // Units
 const version = require('./version')
 const transform = require('./transform')
 const estimators = require('./estimators')
+
+// Custom assertations
+test.Test.prototype.transformEqual = function (actual, expected, message) {
+  this._assert(almostEqual(actual, expected), {
+    message: message || 'transform should have correct elements',
+    operator: 'transformEqual',
+    actual: actual,
+    expected: expected
+  });
+};
 
 test('nudged', (t) => {
   t.test('nudged.version', version)
