060964ba6902ee5aba9ddd92a2919901863705f2
Akseli Palen <akseli.palen@gmail.com>
2021-06-07 22:03:38 +0300

    Unit test for transform.almostEqual

--- test/transform/index.js
+++ test/transform/index.js
@@ -1,4 +1,5 @@
 // A unit for each method.
+const almostEqual = require('./almostEqual')
 const create = require('./create')
 const createFromArray = require('./createFromArray')
 const createFromPolar = require('./createFromPolar')
@@ -6,6 +7,7 @@ const epsilon = require('./epsilon')
 const equal = require('./equal')
 
 module.exports = (t) => {
+  t.test('nudged.transform.almostEqual', almostEqual)
   t.test('nudged.transform.create', create)
   t.test('nudged.transform.createFromArray', createFromArray)
   t.test('nudged.transform.createFromPolar', createFromPolar)
