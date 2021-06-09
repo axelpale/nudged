3b3abd9a0525ad20185ea601cc78a45b2c9f98d4
Akseli Palen <akseli.palen@gmail.com>
2021-06-09 13:04:34 +0300

    Rename transform to affine

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-exports.transform = require('./lib/transform')
+exports.affine =
+exports.transform = require('./lib/affine')
+
 exports.estimators = require('./lib/estimators')
+
 exports.estimate = require('./lib/estimate')
+
 exports.version = require('./lib/version')
