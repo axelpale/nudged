cba389040e277b141f9182c085afac666a5fc263
Akseli Palen <akseli.palen@gmail.com>
2021-06-09 13:11:17 +0300

    Revert "Rename transform to affine"
    This reverts commit 3b3abd9a0525ad20185ea601cc78a45b2c9f98d4.

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-exports.affine =
-exports.transform = require('./lib/affine')
-
+exports.transform = require('./lib/transform')
 exports.estimators = require('./lib/estimators')
-
 exports.estimate = require('./lib/estimate')
-
 exports.version = require('./lib/version')
