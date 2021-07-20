317a8ca76e9dd8fab802e84b412f20e9bd4a07fa
Akseli Palen <akseli.palen@gmail.com>
2021-07-20 14:50:29 +0300

    Move epsilon from transform to root namespace

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+// Constants
+exports.epsilon = require('./lib/epsilon')
+exports.version = require('./lib/version')
+
+// Estimators
 exports.estimators = require('./lib/estimators')
 exports.estimate = require('./lib/estimate')
+
+// Geometries
 exports.point = require('./lib/point')
 exports.transform = require('./lib/transform')
-exports.version = require('./lib/version')
