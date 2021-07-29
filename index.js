2616411a311ee628cca8a88ab3bada643ab1916c
Akseli Palen <akseli.palen@gmail.com>
2021-07-29 18:53:54 +0300

    Move root module index under lib

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-// Constants
-exports.epsilon = require('./lib/epsilon')
-exports.version = require('./lib/version')
-
-// Geometries
-exports.point = require('./lib/point')
-exports.transform = require('./lib/transform')
-
-// Estimators
-exports.estimators = require('./lib/estimators')
-exports.estimate = require('./lib/estimate')
-
-// Tools
-exports.analysis = require('./lib/analysis')
+module.exports = require('./lib/index')
