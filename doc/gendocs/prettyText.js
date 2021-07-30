aab62b2131e4e6ba729f83b983b5084bcfd79dd9
Akseli Palen <akseli.palen@gmail.com>
2021-07-30 13:14:31 +0300

    Hide docs generated by genversion

--- doc/gendocs/prettyText.js
+++ doc/gendocs/prettyText.js
@@ -8,8 +8,13 @@ const prettyObjectLiterals = (text) => {
   return text.replace(expressions.objectLiteral, '`{$1}`')
 }
 
+const removeGenversion = (text) => {
+  return text.replace(expressions.genversion, '')
+}
+
 module.exports = (text) => {
   text = prettyObjectLiterals(text)
   text = prettyArrayLiterals(text)
+  text = removeGenversion(text)
   return text
 }
