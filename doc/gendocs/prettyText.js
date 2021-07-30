b8b9d3f638c9d7efed44265bd5df0f13cf42c5e8
Akseli Palen <akseli.palen@gmail.com>
2021-07-30 21:24:21 +0300

    Route keywords such as point and transform

--- doc/gendocs/prettyText.js
+++ doc/gendocs/prettyText.js
@@ -12,9 +12,20 @@ const removeGenversion = (text) => {
   return text.replace(expressions.genversion, '')
 }
 
+const pointKeyword = /(a|optional) point(\W)?/g
+const transformKeyword = /a transform(\W)?/g
+const toleranceKeyword = /nudged\.tolerance(\W)?/g
+const routeKeywords = (text) => {
+  text = text.replace(pointKeyword, '$1 [point](#nudgedpoint)$2')
+  text = text.replace(transformKeyword, 'a [transform](#nudgedtransform)$1')
+  text = text.replace(toleranceKeyword, '[nudged.tolerance](#nudgedtolerance)$1')
+  return text
+}
+
 module.exports = (text) => {
   text = prettyObjectLiterals(text)
   text = prettyArrayLiterals(text)
   text = removeGenversion(text)
+  text = routeKeywords(text)
   return text
 }
